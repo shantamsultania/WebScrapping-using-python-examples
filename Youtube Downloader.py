0961039ca834b4ac9414ef63549b638888e51cba
shantam sultania <48251619+shantamsultania@users.noreply.github.com>
2020-05-30 10:26:56 +0530

    Update Youtube Downloader.py

--- Youtube Downloader.py
+++ Youtube Downloader.py
@@ -3,4 +3,5 @@ import youtube_dl as you
 
 output = {}
 with you.YoutubeDL(output)as out:
+    # just replace the url with any urlof your choice and you are done
     out.download(["https://www.youtube.com/watch?v=lVLDyZCZfh8"])
